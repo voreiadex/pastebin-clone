15db40323bdd07dca564ff0077d59541d2d1e75d
voreiadex <zhigalov.matvey07@gmail.com>
2025-05-12 21:26:43 +0300

    Initial commit

---
+++ pastebin/src/main/java/com/example/pastebin/DotenvConfig.java
@@ -0,0 +1,10 @@
+package com.example.pastebin;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/.env")
+public class DotenvConfig {
+
+}
